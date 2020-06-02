227629bdd74ad5e12856b1726b0d2a7f31b02eb6
PeterCrosta <pscrosta@gmail.com>
2020-06-02 17:25:59 -0400

    decided on info for education, done for day

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+REMINDER:
+Render resume and download it
+
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/components/EducationExperience.js
+++ src/components/EducationExperience.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import columbia from '/Users/petercrosta/Desktop/personal-work/portfolio/src/data/columbia-logo.jpg'
 
 function EducationExperience() {
     return (
-        <div id='education'>
+        <div id='education-experience'>
             <h1>Education and Experience</h1>
+            <div className='education'>
+                <img alt='columbia' src={columbia} className='school-image' />
+                <h3 className='school-name'>Columbia University</h3>
+                <span className='degree'>Premedical Postbaccalaureate,</span> <span className='grad-date'>May, 2018</span>
+                <p className='description'>Placeholder for description of program</p>
+                <span>Coursework included </span> <span className='single-technology'>math, chem, phys, orgo, bio, neuropsychology</span>
+            </div>
         </div>
     )
 }
