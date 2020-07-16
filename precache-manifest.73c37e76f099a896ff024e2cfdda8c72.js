0b473d11f8d718d4dd2490fc574f345b479b9edf
PeterCrosta <pscrosta@gmail.com>
2020-07-16 13:41:23 -0400

    Updates

---
+++ precache-manifest.73c37e76f099a896ff024e2cfdda8c72.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb8ae7b48b29ca55b9b6e8c84415e66",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e7c04b7267088b086a2f",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ebdd85ebfc928747be9b",
+    "url": "/portfolio/static/css/main.ed569bd8.chunk.css"
+  },
+  {
+    "revision": "e7c04b7267088b086a2f",
+    "url": "/portfolio/static/js/2.844eebaf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.844eebaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebdd85ebfc928747be9b",
+    "url": "/portfolio/static/js/main.5e227d45.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "9cc4107250559678ca57515416b70828",
+    "url": "/portfolio/static/media/Crosta Peter Resume 2020 07 06.9cc41072.pdf"
+  },
+  {
+    "revision": "e90be0441cd09fd032cbe12efb1002c4",
+    "url": "/portfolio/static/media/DelveNYC-logo.e90be044.jpg"
+  },
+  {
+    "revision": "2753a09489dda73bfc0ab9796431c064",
+    "url": "/portfolio/static/media/NYU.2753a094.gif"
+  },
+  {
+    "revision": "6b1bcf22cc2c8fb2d508371ec551e5c5",
+    "url": "/portfolio/static/media/columbia-logo.6b1bcf22.jpg"
+  },
+  {
+    "revision": "8dc4e9c0eeabd784989249e27cfa6c2a",
+    "url": "/portfolio/static/media/drugCheck.8dc4e9c0.jpg"
+  },
+  {
+    "revision": "69eeeb7b6d83e184b733208258528856",
+    "url": "/portfolio/static/media/fennel.69eeeb7b.png"
+  },
+  {
+    "revision": "42558764d5dd72bbe213e96dab4a674b",
+    "url": "/portfolio/static/media/fullstack-logo.42558764.png"
+  }
+]);
