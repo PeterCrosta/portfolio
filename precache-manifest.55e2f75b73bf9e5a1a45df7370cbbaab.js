7213f9509c334610cf4965d3e1fc5c7310bce780
PeterCrosta <pscrosta@gmail.com>
2020-07-21 14:14:24 -0400

    Updates

---
+++ precache-manifest.55e2f75b73bf9e5a1a45df7370cbbaab.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "176f94e4bffb1eb140c3a8bdc8ee726e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b298d8f470536e5e8485",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5e6fe74bf53277b1c6b1",
+    "url": "/portfolio/static/css/main.462ef87e.chunk.css"
+  },
+  {
+    "revision": "b298d8f470536e5e8485",
+    "url": "/portfolio/static/js/2.cb73f854.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.cb73f854.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e6fe74bf53277b1c6b1",
+    "url": "/portfolio/static/js/main.255114ef.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "e90be0441cd09fd032cbe12efb1002c4",
+    "url": "/portfolio/static/media/DelveNYC-logo.e90be044.jpg"
+  },
+  {
+    "revision": "2753a09489dda73bfc0ab9796431c064",
+    "url": "/portfolio/static/media/NYU.2753a094.gif"
+  },
+  {
+    "revision": "26ac5c88341e544d623bbeb75175999a",
+    "url": "/portfolio/static/media/Resume 2020 07 06.26ac5c88.pdf"
+  },
+  {
+    "revision": "6b1bcf22cc2c8fb2d508371ec551e5c5",
+    "url": "/portfolio/static/media/columbia-logo.6b1bcf22.jpg"
+  },
+  {
+    "revision": "0e9b338713e4e8f0867f4aa46438afd5",
+    "url": "/portfolio/static/media/drugCheck.0e9b3387.jpeg"
+  },
+  {
+    "revision": "69eeeb7b6d83e184b733208258528856",
+    "url": "/portfolio/static/media/fennel.69eeeb7b.png"
+  },
+  {
+    "revision": "42558764d5dd72bbe213e96dab4a674b",
+    "url": "/portfolio/static/media/fullstack-logo.42558764.png"
+  },
+  {
+    "revision": "a0d8b3aaf52d1bc4d6651d37157831a3",
+    "url": "/portfolio/static/media/webImage.a0d8b3aa.png"
+  }
+]);
