ca1cce21f45d4de203307f715c7b6d1b88f1f89f
PeterCrosta <pscrosta@gmail.com>
2020-07-21 21:43:13 -0400

    updated resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import resumePDF from '/Users/petercrosta/Desktop/personal-work/portfolio/src/data/Resume 2020 07 06.pdf'
+import resumePDF from '/Users/petercrosta/Desktop/personal-work/portfolio/src/data/Resume 2020 07 21.pdf'
 import logo from '/Users/petercrosta/Desktop/personal-work/portfolio/src/data/peter logo white.png'
 
 
