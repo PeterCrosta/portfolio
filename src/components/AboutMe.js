0f5753b3fa26fd3ff421326fb13b7264ecfa66f9
PeterCrosta <pscrosta@gmail.com>
2020-07-24 09:56:18 -0400

    cleaned up css and adjusted harry method

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -13,11 +13,11 @@ class AboutMe extends React.Component {
         this.state = {
             harryArr: [harry1, harry2, harry3, harry4, harry5]
         }
-        this.handleHover = this.handleHover.bind(this)
+        this.handleHarry = this.handleHarry.bind(this)
         this.handleOff = this.handleOff.bind(this)
     }
 
-    handleHover(event) {
+    handleHarry(event) {
         let harry = this.state.harryArr[Math.floor(Math.random()*this.state.harryArr.length)]
         let pic = document.getElementsByClassName('head-shot')
         pic[0].src = harry
@@ -36,7 +36,7 @@ class AboutMe extends React.Component {
                         <p className='description'>
                         My name is Peter Crosta. I am a full stack software engineer with past lives in political campaign management and neurology research. I recently graduated from Fullstack Academy, which focuses on the NERDS stack (Node, Express, React/Redux, and database using SQL). After graduation, I was invited to work with the incoming cohort of students as a teaching fellow. I love readable code, learning and leveraging the advantages of a new technology, and sharing my knowledge with my peers. When I’m not coding you will find me taking flying trapeze lessons, cooking for friends, or reading on the couch with my dog, 
                         <span> </span>
-                        <span className='harry-text' onMouseEnter={this.handleHover} onMouseLeave={this.handleOff}>Harry</span>.
+                        <span className='harry-text' onMouseEnter={this.handleHarry} onMouseLeave={this.handleOff} onClick={this.handleHarry} >Harry</span>.
                         </p>
                         <p className='description'>
                         I am currently open to job opportunities, contract, and freelance work. If  you think I would be a good fit, don’t hesitate to reach out!
