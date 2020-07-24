0d2e34292d9a7bab2630526ab6f2dcdd6f6e4d5a
PeterCrosta <pscrosta@gmail.com>
2020-07-24 09:37:45 -0400

    full work history added

--- src/data/workHistory.js
+++ src/data/workHistory.js
@@ -4,7 +4,7 @@ const workHistory = [
         title: "Software Engineering Teaching Fellow",
         dates: ["March 2020", "May 2020"],
         location: "New York, NY",
-        resp: ["Acted as project manager of student projects, leading agile work environment", "Led projects' daily stand-ups, weekly code reviews, helped debug recalcitrant issues, and consulted on applications' designs and implementations", "Conducted 2-4 technical interviews per week for prospective students"]
+        resp: ["Acted as project manager of student projects, leading agile work environment", "Led project daily stand-ups, weekly code reviews, helped debug recalcitrant issues, and consulted on application designs and implementations", "Conducted 2-4 technical interviews per week for prospective students"]
     },
     {
         org: "Columbia Antiepileptic Drug Database",
@@ -19,6 +19,41 @@ const workHistory = [
         dates: ["Dec 2011", "Aug 2015"],
         location: "New York, NY",
         resp: ["Ensured advertisements met technical quality standards and were consistent with intended technical attributes", "Flagged advertisements with potentially controversial or ethically dubious content", "Wrote policies to streamline the auditing process"]
+    },
+    {
+        org: "New York Presbyterian Emergency Department",
+        title: "Research Associate",
+        dates: ["Sept 2015", "Aug 2016"],
+        location: "New York, NY",
+        resp: ["Identified candidates for diverse research studies", "Explained confidentiality and consent forms to patients", "Assembled equipment for outpatient procedures", "HIPAA Certified"]
+    },
+    {
+        org: "City College, CUNY",
+        title: "Teacher’s Assistant",
+        dates: ["Aug 2014", "Aug 2014"],
+        location: "New York, NY",
+        resp: ["Instructed students in trigonometry and calculus for a calculus bridge course", "Tracked students’ progress", "Wrote answer keys and graded exams"]
+    },
+    {
+        org: "Jonathan Geballe for District Leader",
+        title: "Campaign Manager",
+        dates: ["April 2013", "Sept 2013"],
+        location: "New York, NY",
+        resp: ["Coordinated volunteers and materials to staff phone banks, canvassing, and petitioning", "Wrote copy for and oversaw the design and distribution of direct mailers and advertising in local press", "Drafted op-eds, press releases, and personal greetings for candidate’s signature", "Managed the candidate’s schedule in conjunction with other campaigns and elected officials", "Administered a single budget drawing from three separate campaign accounts"]
+    },
+    {
+        org: "Office of Senator Kirsten Gillibrand/Gillibrand for Senate",
+        title: "Outreach Intern",
+        dates: ["May 2011", "Feb 2012"],
+        location: "New York, NY",
+        resp: ["Drafted briefs to prepare the Senator and senior staff for meetings and public events", "Created databases on advocacy groups, local officials, and local press", "Researched efficacy of new policy initiatives", "Drafted op-eds, press releases, greeting letters, letters to political clubs, and letters of support for grant money", "Researched donors and prospective challengers", "Advanced events for the Senator"]
+    },
+    {
+        org: "Governor Arnold Schwarzenegger Administration",
+        title: "Executive Correspondence Intern",
+        dates: ["June 2009", "Aug 2009"],
+        location: "Sacramento, CA",
+        resp: ["Drafted correspondence for Governor’s signature to business and government VIPs", "Researched individuals and monitored news to determine who would receive personal correspondence from Governor Schwarzenegger", "Coordinated information from a variety of agencies within the administration"]
     }
 ]
 
